bc565e3e8d1e8e4052c8ce38dc0dfc2cce83fc60
JeanneGrenet <jeanne.grenet@gmail.com>
2024-05-21 16:26:18 +0200

    create form

--- synergie4d-front/app/Selection.tsx
+++ synergie4d-front/app/Selection.tsx
@@ -1,21 +1,51 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Selection() {
-  const styles = ["Professionel", "Décontracté", "Sportif", "Chic"];
+  const styles = ["Chic", "Décontracté", "Professionel", "Sportif"];
+  const [selectedStyle, setSelectedStyle] = useState<string>();
+  const [city, setCity] = useState("");
+
+  const handleSubmit = async (e: HTMLFormElement) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ city, selectedStyle }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="py-6 px-4">
-      <div className="flex flex-col gap-4">
-        <Input placeholder="Ville" />
-        <div className="grid grid-cols-2 gap-4">
-          {styles.map((style) => (
-            <Button key={style} variant="outline" size="sm">
-              {style}
-            </Button>
-          ))}
+      <form onSubmit={() => handleSubmit}>
+        <div className="flex flex-col gap-4">
+          <Input
+            placeholder="Ville"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <div className="grid grid-cols-2 gap-4">
+            {styles.map((style) => (
+              <Button
+                key={style}
+                variant="outline"
+                size="sm"
+                onClick={() => setSelectedStyle(style)}
+              >
+                {style}
+              </Button>
+            ))}
+          </div>
+          <Button type="submit">Envoyer</Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
