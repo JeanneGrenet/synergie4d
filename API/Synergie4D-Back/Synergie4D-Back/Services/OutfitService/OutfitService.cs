f5ed91ae04dc1057b33c11d54d19b529b7f711c0
AntoineDI23 <antoine.simeon@viacesi.fr>
2024-05-22 15:32:30 +0200

    [Feat] Add slugs for outfits examples

--- API/Synergie4D-Back/Synergie4D-Back/Services/OutfitService/OutfitService.cs
+++ API/Synergie4D-Back/Synergie4D-Back/Services/OutfitService/OutfitService.cs
@@ -11,49 +11,57 @@ namespace Synergie4D_Back.Services.OutfitService
                 Id = 1,
                 Description = "T-shirt / jean / basket",
                 Style = Style.Relaxed,
-                Weather = Temperature.Hot
+                Weather = Temperature.Hot,
+                Slug = "t-shirt-jean-basket"
             },
             new() {
                 Id = 2,
                 Description = "T-shirt / sweat / jean / basket",
                 Style = Style.Relaxed,
-                Weather = Temperature.Cold
+                Weather = Temperature.Cold,
+                Slug = "t-shirt-sweat-jean-basket"
             },
             new() {
                 Id = 3,
                 Description = "Chemise / pantalon / chaussure de ville",
                 Style = Style.Professional,
-                Weather = Temperature.Cold
+                Weather = Temperature.Cold,
+                Slug = "chemise-pantalon-chaussure-de-ville"
             },
             new() {
                 Id = 4,
                 Description = "Polo / pantalon / basket",
                 Style = Style.Professional,
-                Weather = Temperature.Hot
+                Weather = Temperature.Hot,
+                Slug = "polo-pantalon-basket"
             },
             new() {
                 Id = 5,
                 Description = "T-shirt / short / basket",
                 Style = Style.Sporty,
-                Weather = Temperature.Hot
+                Weather = Temperature.Hot,
+                Slug = "t-shirt-short-basket"
             },
             new() {
                 Id = 6,
                 Description = "Costume / chaussure de ville",
                 Style = Style.Professional,
-                Weather = Temperature.Cold
+                Weather = Temperature.Cold,
+                Slug = "costume-chaussure-de-ville"
             },
             new() {
                 Id = 7,
                 Description = "T-shirt / pull / pantalon / chaussure de ville",
                 Style = Style.Relaxed,
-                Weather = Temperature.Cold
+                Weather = Temperature.Cold,
+                Slug = "t-shirt-pull-pantalon-chaussure-de-ville"
             },
             new() {
                 Id = 8,
                 Description = "T-shirt / sweat / jogging / basket",
                 Style = Style.Sporty,
-                Weather = Temperature.Hot
+                Weather = Temperature.Hot,
+                Slug = "t-shirt-sweat-jogging-basket"
             }
         ];
 
