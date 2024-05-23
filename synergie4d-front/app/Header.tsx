5451298b1ca0727da7820b334aa24c474340971d
JeanneGrenet <jeanne.grenet@gmail.com>
2024-05-23 14:38:10 +0200

    add shadow on header

--- synergie4d-front/app/Header.tsx
+++ synergie4d-front/app/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="bg-white py-4 flex justify-center border-b border-primary">
+    <div className="bg-white py-4 flex justify-center shadow-md">
       <Image src="/logo.svg" width={350} height={50} alt="logo"/>
     </div>
   );
